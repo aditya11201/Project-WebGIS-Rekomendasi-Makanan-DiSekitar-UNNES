55783270199ed8a8a42549c50ea203d13f1a8fe2
aditya11201 <64565901+aditya11201@users.noreply.github.com>
2023-06-09 18:21:13 +0700

    Add files via upload
    9 Juni 1821

--- script.js
+++ script.js
@@ -25,49 +25,6 @@ var mwrpenyet = L.marker([-7.047224871391219, 110.38991797128035]).bindPopup('Wr
 /* Pop Up */
 
 /* Pop Up */
-var markerIcon = L.icon({
-  iconUrl: 'https://lh5.googleusercontent.com/p/AF1QipPCTqbmer8qaIq3xSb6RA6rgM19uUJCHpmcmvot=w114-h86-k-no',
-  iconSize: [30, 30], // ukuran gambar logo marker
-  iconAnchor: [16, 32] // titik anchor pada gambar logo marker
-});
-
-var marker = L.marker([-7.057946668691204, 110.39740750550186], {
-  title: "Bakso dan Mie Ayam Bosku",
-  icon: markerIcon
-}).addTo(tmptmknenak);
-
-marker.on('click', function () {
-
-  var win = L.control.window(map, {
-    title: 'Bakso dan Mie Ayam Bosku',
-    maxWidth: 800,
-    modal: true
-  });
-
-  win.content(
-    '<img src="https://lh5.googleusercontent.com/p/AF1QipPCTqbmer8qaIq3xSb6RA6rgM19uUJCHpmcmvot=w408-h306-k-no" alt="Image">' +
-    '<h2>Daftar Harga</h2>' +
-    '<p><a href="https://www.google.com/maps/place/Bakso+dan+Mie+Ayam+Bosku/@-7.0579994,110.3973732,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNoI3it7ydj9_dshRotEklm5dYBHAYAozG0ZQI2!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNoI3it7ydj9_dshRotEklm5dYBHAYAozG0ZQI2%3Dw203-h360-k-no!7i1080!8i1920!4m20!1m10!3m9!1s0x2e70896cfc62ef6d:0xd10f4c915ad7e587!2sBakso+dan+Mie+Ayam+Bosku!8m2!3d-7.0579994!4d110.3973732!10e5!14m1!1BCgIgAQ!16s%2Fg%2F11f61ggnkt!3m8!1s0x2e70896cfc62ef6d:0xd10f4c915ad7e587!8m2!3d-7.0579994!4d110.3973732!10e5!14m1!1BCgIYIQ!16s%2Fg%2F11f61ggnkt?entry=ttu#" target="_blank">Lihat daftar harga</a></p>' +
-    '<h2>Informasi Lokasi</h2>' +
-    '<p><strong>Nama:</strong> Jude Bellingham</p>' +
-    '<p><strong>Alamat:</strong> W9RW+RW5, Patemon, Kec. Gn. Pati, Kota Semarang, Jawa Tengah 50228</p>' +
-    '<p><strong>Koordinat:</strong> -7.057946668691204, 110.39740750550186</p>' +
-    '<h2>Google Maps</h2>' +
-    '<button onclick="window.open(\'https://www.google.com/maps?q=-7.057946668691204,110.39740750550186\')" target="_blank">Lihat di Google Maps</button>' +
-    '<h2>Deskripsi</h2>' +
-    '<p>Bakso The best di daerah unnes rame setiap mau makan rame teruss</p>'
-  );
-
-  win.prompt({
-    callback: function () {
-      alert('Klik logo ikonnya lagi yak kalau mau liat info :)');
-    }
-  });
-
-  win.show();
-});
-
-
 
 /* Pop Up */
 /* Layer Satelite */
