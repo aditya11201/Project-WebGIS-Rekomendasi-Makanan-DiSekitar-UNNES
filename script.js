b073ee26ab9858c8bad0d3b85133c99e986bc7fe
aditya11201 <64565901+aditya11201@users.noreply.github.com>
2023-06-09 23:14:09 +0700

    Add files via upload
    juni 09 23:13

--- script.js
+++ script.js
@@ -7,26 +7,13 @@ var Stamen_TonerLabels = L.tileLayer(
     ext: 'png'
   });
 var tmptmknenak = L.layerGroup();
-var mnasgor = L.marker({
-  iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png',
-  shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png',
 
-  iconSize: [38, 95], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62], // the same for the shadow
-  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
 var mburjo = L.marker([-7.056004468211421, 110.39658764120955]).bindPopup('Burjo Laskar 2').addTo(tmptmknenak);
 var mnasgor = L.marker([-7.055878173266243, 110.3947063596325]).bindPopup('Nasi Goreng Berhias Brebes').addTo(
   tmptmknenak);
 var mwrpenyet = L.marker([-7.047224871391219, 110.38991797128035]).bindPopup('WrpenyetLombokRawitUnnes').addTo(
   tmptmknenak);
-/* Pop Up */
 
-/* Pop Up */
-
-/* Pop Up */
 /* Layer Satelite */
 var mbAttr =
   'Map data & copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
